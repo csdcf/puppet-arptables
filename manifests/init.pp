2638b7b60cd373d8304a35fec4e0336ece927da0
Miles Davis <miles@cs.stanford.edu>
2012-07-25 07:05:17 -0700

    Copied from existing arptables_jf module

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,7 @@
 # Class: arptables
 #
 # This module manages arptables
+# Userspace control program for the arptables network filter
 #
 # Parameters:
 #
@@ -9,9 +10,33 @@
 # Requires:
 #
 # Sample Usage:
+# 
+#    $suppress_arp = [ '171.64.64.64', '171.64.64.179' ]
+#
+#    include arptables_jf
 #
 # [Remember: No empty lines between comments and class definition]
-class arptables {
+class arptables ($suppress_arp = [] ) {
+
+    package { 
+        'arptables_jf': ensure => present;
+    }
+
+    file { 
+        '/etc/sysconfig/arptables':
+            content => template("arptables_jf/arptables.erb"),
+            #'puppet:///arptables_jf/arptables',
+            owner => 'root', group => 'root',
+            notify => Service['arptables_jf'],
+            ensure => 'file';
+    }
+
+    service { 
+        'arptables_jf':
+            enable => 'true';
+    }
+
+}
 
 
 }
